f0d5c95e837c22352932f0f847a75b8645295b35
Jose Raul Arita-Escalante <escalantejra@vcu.edu>
2022-09-19 13:41:37 -0700

    adjusted directory for MCRaT output res. level 5

--- Src/mcrat_input.h
+++ Src/mcrat_input.h
@@ -49,7 +49,7 @@
 #define SIMULATION_TYPE SCIENCE
 #define FILEPATH "/nobackup/jaritaes/CHOMBO_LOWER_BEG_RES/"
 #define FILEROOT "data."
-#define MC_PATH "SCIENCE-levs/1/"
+#define MC_PATH "SCIENCE-levs/5/"
 #define     SIM_SWITCH                  PLUTO_CHOMBO
 #define     GEOMETRY                    SPHERICAL
 #define     DIMENSIONS                  TWO
